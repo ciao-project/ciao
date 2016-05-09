c7afc6283da22910d23abcb5f0f9da2f767155d8
Tim Pepper <timothy.c.pepper@linux.intel.com>
2016-05-09 11:06:14 -0700

    ciao-scheduler: minor documentation edits
    Just cleaning up a few things for better readability and consistency.
    
    Signed-off-by: Tim Pepper <timothy.c.pepper@linux.intel.com>

--- ciao-scheduler/doc.go
+++ ciao-scheduler/doc.go
@@ -17,7 +17,7 @@
 /*
 
 ciao-scheduler is the SSNTP server that runs in the control plane to
-broker a CIAO based cloud.
+broker a ciao based cloud.
 
 In addressing the broader problem of dispatching a workload for a
 user, ciao splits the problem:
@@ -88,11 +88,11 @@ and that in turn implies there is indeed capacity for new work.
 
 Finding the best fit is more important if resources are highly constrained
 and you want to make an attempt to give future workloads (whose specific
-nature is yet unknown) a better chance of succeeding.  Again, though
+nature is yet unknown) a better chance of succeeding.  Again though,
 attempting to address future unknowns adds complexity to the code,
 incurs latencies and hinders scalability.
 
-Today a compute node that has no remaining capacity (module a buffer
+Today a compute node that has no remaining capacity (modulo a buffer
 amount for the launcher and host OS's stability) will report that
 it is full and the scheduler will not dispatch work to that node.
 As a last resort, ciao-scheduler will return a "cloud full" status to
@@ -101,16 +101,16 @@ ciao-controller if no compute nodes have capacity to do work.
 Data Structures and Scale
 
 In the initial implementation, the scheduling choice
-focuses primarily on RAM, disk and CPU availability
-(see the "Resource" enumeration type in the START payload
-(https://github.com/01org/ciao/blob/master/payloads/start.go) for more
+focuses primarily on RAM, disk and CPU availability (see
+the "Resource" enumeration type in the START payload at
+https://github.com/01org/ciao/blob/master/payloads/start.go for more
 details) on compute nodes relative to the requested workload start.
-This list of tracked resource types is will grow over time to encompass
+This list of tracked resource types will grow over time to encompass
 many more compute node and workload characteristics.  We don't expect
 that to significantly impact the time needed to make a scheduling choice.
 We have designed throughout ciao to scale.
 
-Our goal is to have scheduling take on the order of microseconds.
+Our goal is to make scheduling choices in the order of microseconds.
 While we haven't yet tested on extremely large clusters, conceptually one
 should expect that searching an in-memory data structure containing many
 thousands of nodes' resource data should not take more than milliseconds.
