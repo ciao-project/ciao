ciao-project/ciao

commit a17d38a2eaf2b96867addef95343229ce58d88c3

ciao-scheduler: Send connection events to the controller
When we receive nodes connection and disconnection SSNTP
notifications, we now send respectively a NodeConnected
and NodeDisconnected events to the controller.

The controller will now be able to update its stats and
nodes tracking data from explicit events and not on
implicit STATS payloads.

FIXES: issues/12

Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

ciao-scheduler/scheduler.go
@@ -83,6 +83,56 @@ type controllerStat struct {
 	uuid   string
 }
 
+func (sched *ssntpSchedulerServer) sendNodeConnectionEvent(nodeUUID, controllerUUID string, nodeType payloads.Resource, connected bool) (int, error) {
+	if connected == true {
+		payload := payloads.NodeConnected{
+			Connected: payloads.NodeConnectedEvent{
+				NodeUUID: nodeUUID,
+				NodeType: nodeType,
+			},
+		}
+
+		b, err := yaml.Marshal(&payload)
+		if err != nil {
+			return 0, err
+		}
+
+		return sched.ssntp.SendEvent(controllerUUID, ssntp.NodeConnected, b)
+	} else {
+		payload := payloads.NodeDisconnected{
+			Disconnected: payloads.NodeConnectedEvent{
+				NodeUUID: nodeUUID,
+				NodeType: nodeType,
+			},
+		}
+
+		b, err := yaml.Marshal(&payload)
+		if err != nil {
+			return 0, err
+		}
+
+		return sched.ssntp.SendEvent(controllerUUID, ssntp.NodeDisconnected, b)
+	}
+}
+
+func (sched *ssntpSchedulerServer) sendNodeConnectedEvents(nodeUUID string, nodeType payloads.Resource) {
+	sched.controllerMutex.Lock()
+	defer sched.controllerMutex.Unlock()
+
+	for _, c := range sched.controllerMap {
+		sched.sendNodeConnectionEvent(nodeUUID, c.uuid, nodeType, true)
+	}
+}
+
+func (sched *ssntpSchedulerServer) sendNodeDisconnectedEvents(nodeUUID string, nodeType payloads.Resource) {
+	sched.controllerMutex.Lock()
+	defer sched.controllerMutex.Unlock()
+
+	for _, c := range sched.controllerMap {
+		sched.sendNodeConnectionEvent(nodeUUID, c.uuid, nodeType, false)
+	}
+}
+
 func (sched *ssntpSchedulerServer) ConnectNotify(uuid string, role uint32) {
 	switch role {
 	case ssntp.Controller:
@@ -124,6 +174,8 @@ func (sched *ssntpSchedulerServer) ConnectNotify(uuid string, role uint32) {
 		node.uuid = uuid
 		sched.cnList = append(sched.cnList, &node)
 		sched.cnMap[uuid] = &node
+
+		sched.sendNodeConnectedEvents(uuid, payloads.ComputeNode)
 	case ssntp.NETAGENT:
 		sched.nnMutex.Lock()
 		defer sched.nnMutex.Unlock()
@@ -137,13 +189,14 @@ func (sched *ssntpSchedulerServer) ConnectNotify(uuid string, role uint32) {
 		node.status = ssntp.CONNECTED
 		node.uuid = uuid
 		sched.nnMap[uuid] = &node
+
+		sched.sendNodeConnectedEvents(uuid, payloads.NetworkNode)
 	}
 
 	glog.V(2).Infof("Connect (role 0x%x, uuid=%s)\n", role, uuid)
 }
 
 func (sched *ssntpSchedulerServer) DisconnectNotify(uuid string) {
-
 	sched.controllerMutex.Lock()
 	defer sched.controllerMutex.Unlock()
 	if sched.controllerMap[uuid] != nil {
@@ -188,6 +241,9 @@ func (sched *ssntpSchedulerServer) DisconnectNotify(uuid string) {
 			sched.cnMRUIndex = -1
 		}
 
+		/* We need a go routine as the controller lock is taken */
+		go sched.sendNodeDisconnectedEvents(uuid, payloads.ComputeNode)
+
 		delete(sched.cnMap, uuid)
 
 		glog.V(2).Infof("Disconnect cn (uuid=%s)\n", uuid)
@@ -197,6 +253,9 @@ func (sched *ssntpSchedulerServer) DisconnectNotify(uuid string) {
 	sched.nnMutex.Lock()
 	defer sched.nnMutex.Unlock()
 	if sched.nnMap[uuid] != nil {
+		/* We need a go routine as the controller lock is taken */
+		go sched.sendNodeDisconnectedEvents(uuid, payloads.NetworkNode)
+
 		//TODO: consider moving to nnInactiveMap?
 		delete(sched.nnMap, uuid)
 

commit 7be861507e5f51491b01e61d2ebcb375fbd96752

ciao-scheduler: add client connect/disconnect test
Call the controller, agent, and netagent connect/disconnect functions
repeatedly to show they work and that the locking does not deadlock.  This
test should have shown the deadlock introduced back in commit a17d38a.

Signed-off-by: Tim Pepper <timothy.c.pepper@linux.intel.com>

ciao-scheduler/scheduler.go
@@ -164,7 +164,7 @@ func (sched *ssntpSchedulerServer) sendNodeDisconnectedEvents(nodeUUID string, n
 
 // Add state for newly connected Controller
 // This function is symmetric with disconnectController().
-func (sched *ssntpSchedulerServer) connectController(uuid string) {
+func connectController(sched *ssntpSchedulerServer, uuid string) {
 	sched.controllerMutex.Lock()
 	defer sched.controllerMutex.Unlock()
 
@@ -192,7 +192,7 @@ func (sched *ssntpSchedulerServer) connectController(uuid string) {
 
 // Undo previous state additions for departed Controller
 // This function is symmetric with connectController().
-func (sched *ssntpSchedulerServer) disconnectController(uuid string) {
+func disconnectController(sched *ssntpSchedulerServer, uuid string) {
 	sched.controllerMutex.Lock()
 	defer sched.controllerMutex.Unlock()
 
@@ -236,7 +236,7 @@ func (sched *ssntpSchedulerServer) disconnectController(uuid string) {
 
 // Add state for newly connected Compute Node
 // This function is symmetric with disconnectComputeNode().
-func (sched *ssntpSchedulerServer) connectComputeNode(uuid string) {
+func connectComputeNode(sched *ssntpSchedulerServer, uuid string) {
 	sched.cnMutex.Lock()
 	defer sched.cnMutex.Unlock()
 
@@ -256,7 +256,7 @@ func (sched *ssntpSchedulerServer) connectComputeNode(uuid string) {
 
 // Undo previous state additions for departed Compute Node
 // This function is symmetric with connectComputeNode().
-func (sched *ssntpSchedulerServer) disconnectComputeNode(uuid string) {
+func disconnectComputeNode(sched *ssntpSchedulerServer, uuid string) {
 	sched.cnMutex.Lock()
 	defer sched.cnMutex.Unlock()
 
@@ -287,7 +287,7 @@ func (sched *ssntpSchedulerServer) disconnectComputeNode(uuid string) {
 
 // Add state for newly connected Network Node
 // This function is symmetric with disconnectNetworkNode().
-func (sched *ssntpSchedulerServer) connectNetworkNode(uuid string) {
+func connectNetworkNode(sched *ssntpSchedulerServer, uuid string) {
 	sched.nnMutex.Lock()
 	defer sched.nnMutex.Unlock()
 
@@ -306,7 +306,7 @@ func (sched *ssntpSchedulerServer) connectNetworkNode(uuid string) {
 
 // Undo previous state additions for departed Network Node
 // This function is symmetric with connectNetworkNode().
-func (sched *ssntpSchedulerServer) disconnectNetworkNode(uuid string) {
+func disconnectNetworkNode(sched *ssntpSchedulerServer, uuid string) {
 	sched.nnMutex.Lock()
 	defer sched.nnMutex.Unlock()
 
@@ -323,11 +323,11 @@ func (sched *ssntpSchedulerServer) disconnectNetworkNode(uuid string) {
 func (sched *ssntpSchedulerServer) ConnectNotify(uuid string, role uint32) {
 	switch role {
 	case ssntp.Controller:
-		sched.connectController(uuid)
+		connectController(sched, uuid)
 	case ssntp.AGENT:
-		sched.connectComputeNode(uuid)
+		connectComputeNode(sched, uuid)
 	case ssntp.NETAGENT:
-		sched.connectNetworkNode(uuid)
+		connectNetworkNode(sched, uuid)
 	}
 
 	glog.V(2).Infof("Connect (role 0x%x, uuid=%s)\n", role, uuid)
@@ -336,11 +336,11 @@ func (sched *ssntpSchedulerServer) ConnectNotify(uuid string, role uint32) {
 func (sched *ssntpSchedulerServer) DisconnectNotify(uuid string, role uint32) {
 	switch role {
 	case ssntp.Controller:
-		sched.disconnectController(uuid)
+		disconnectController(sched, uuid)
 	case ssntp.AGENT:
-		sched.disconnectComputeNode(uuid)
+		disconnectComputeNode(sched, uuid)
 	case ssntp.NETAGENT:
-		sched.disconnectNetworkNode(uuid)
+		disconnectNetworkNode(sched, uuid)
 	}
 
 	glog.V(2).Infof("Connect (role 0x%x, uuid=%s)\n", role, uuid)

ciao-scheduler/export_test.go
@@ -21,3 +21,10 @@ type NodeStat nodeStat
 type WorkResources workResources
 
 var PickComputeNode = pickComputeNode
+
+var ConnectController = connectController
+var DisconnectController = disconnectController
+var ConnectComputeNode = connectComputeNode
+var DisconnectComputeNode = disconnectComputeNode
+var ConnectNetworkNode = connectNetworkNode
+var DisconnectNetworkNode = disconnectNetworkNode

ciao-scheduler/scheduler_internal_test.go
@@ -22,6 +22,7 @@ import (
 	"github.com/01org/ciao/payloads"
 	"github.com/01org/ciao/ssntp"
 	"os"
+	"sync"
 	"testing"
 )
 
@@ -337,4 +338,156 @@ func TestHeartBeat(t *testing.T) {
 	if beatTxt != expectedWithHeader {
 		t.Errorf("expected:\n\"%s\"\ngot:\n\"%s\"", expectedWithHeader, beatTxt)
 	}
+}
+
+func controllerMods() {
+	// controller in and out
+	ConnectController(sched, "1")
+	DisconnectController(sched, "1")
+
+	// controller master and two backups
+	ConnectController(sched, "1")
+	ConnectController(sched, "2")
+	ConnectController(sched, "3")
+
+	// remove a backup
+	DisconnectController(sched, "3")
+
+	// remove master
+	DisconnectController(sched, "1")
+
+	// remove last
+	DisconnectController(sched, "2")
+}
+func computeNodeMods() {
+	// compute node in and out
+	ConnectComputeNode(sched, "1")
+	DisconnectComputeNode(sched, "1")
+
+	// multiple compute nodes in various orders
+	ConnectComputeNode(sched, "1")
+	ConnectComputeNode(sched, "2")
+	ConnectComputeNode(sched, "3")
+	ConnectComputeNode(sched, "4")
+	DisconnectComputeNode(sched, "1")
+	DisconnectComputeNode(sched, "2")
+	DisconnectComputeNode(sched, "3")
+	DisconnectComputeNode(sched, "4")
+	ConnectComputeNode(sched, "1")
+	ConnectComputeNode(sched, "2")
+	ConnectComputeNode(sched, "3")
+	ConnectComputeNode(sched, "4")
+	DisconnectComputeNode(sched, "4")
+	DisconnectComputeNode(sched, "3")
+	DisconnectComputeNode(sched, "2")
+	DisconnectComputeNode(sched, "1")
+	ConnectComputeNode(sched, "1")
+	ConnectComputeNode(sched, "2")
+	ConnectComputeNode(sched, "3")
+	ConnectComputeNode(sched, "4")
+	DisconnectComputeNode(sched, "3")
+	DisconnectComputeNode(sched, "1")
+	DisconnectComputeNode(sched, "4")
+	DisconnectComputeNode(sched, "2")
+}
+func networkNodeMods() {
+	// network node in and out
+	ConnectNetworkNode(sched, "1")
+	DisconnectNetworkNode(sched, "1")
+
+	// multiple network nodes in various orders
+	ConnectNetworkNode(sched, "1")
+	ConnectNetworkNode(sched, "2")
+	ConnectNetworkNode(sched, "3")
+	ConnectNetworkNode(sched, "4")
+	DisconnectNetworkNode(sched, "1")
+	DisconnectNetworkNode(sched, "2")
+	DisconnectNetworkNode(sched, "3")
+	DisconnectNetworkNode(sched, "4")
+	ConnectNetworkNode(sched, "1")
+	ConnectNetworkNode(sched, "2")
+	ConnectNetworkNode(sched, "3")
+	ConnectNetworkNode(sched, "4")
+	DisconnectNetworkNode(sched, "4")
+	DisconnectNetworkNode(sched, "3")
+	DisconnectNetworkNode(sched, "2")
+	DisconnectNetworkNode(sched, "1")
+	ConnectNetworkNode(sched, "1")
+	ConnectNetworkNode(sched, "2")
+	ConnectNetworkNode(sched, "3")
+	ConnectNetworkNode(sched, "4")
+	DisconnectNetworkNode(sched, "3")
+	DisconnectNetworkNode(sched, "1")
+	DisconnectNetworkNode(sched, "4")
+	DisconnectNetworkNode(sched, "2")
+}
+
+func clientMiscMods() {
+	/* various interleaved ******************************/
+	ConnectNetworkNode(sched, "a")
+	ConnectComputeNode(sched, "1")
+	ConnectController(sched, "1")
+	DisconnectController(sched, "1")
+	DisconnectComputeNode(sched, "1")
+	DisconnectNetworkNode(sched, "a")
+	ConnectNetworkNode(sched, "a")
+	ConnectComputeNode(sched, "1")
+	ConnectNetworkNode(sched, "b")
+	ConnectController(sched, "c1")
+	DisconnectController(sched, "c1")
+	ConnectComputeNode(sched, "2")
+	DisconnectComputeNode(sched, "1")
+	DisconnectNetworkNode(sched, "a")
+	ConnectController(sched, "c1")
+	ConnectController(sched, "c2")
+	ConnectComputeNode(sched, "3")
+	ConnectComputeNode(sched, "4")
+	ConnectComputeNode(sched, "5")
+	DisconnectComputeNode(sched, "2")
+	DisconnectNetworkNode(sched, "b")
+	DisconnectController(sched, "c2")
+	DisconnectController(sched, "c1")
+	DisconnectComputeNode(sched, "3")
+	DisconnectComputeNode(sched, "4")
+	DisconnectComputeNode(sched, "5")
+}
+
+// TestClientMgmtLocking should run to completion without deadlocking or
+// panic'ing.  If it does not, "go test -race" should highlight the
+// problem.
+func TestClientMgmtLocking(t *testing.T) {
+	var wg sync.WaitGroup
+
+	sched = configSchedulerServer()
+	if sched == nil {
+		t.Fatal("unable to configure test scheduler")
+	}
+
+	// simple first serial sanity check
+	controllerMods()
+	computeNodeMods()
+	networkNodeMods()
+	clientMiscMods()
+
+	// now in parallel
+	wg.Add(3)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 100000; i++ {
+			controllerMods()
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 100000; i++ {
+			computeNodeMods()
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 100000; i++ {
+			networkNodeMods()
+		}
+	}()
+	wg.Wait()
 }