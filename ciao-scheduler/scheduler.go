ciao-project/ciao

commit a17d38a2eaf2b96867addef95343229ce58d88c3

ciao-scheduler: Send connection events to the controller
When we receive nodes connection and disconnection SSNTP
notifications, we now send respectively a NodeConnected
and NodeDisconnected events to the controller.

The controller will now be able to update its stats and
nodes tracking data from explicit events and not on
implicit STATS payloads.

FIXES: issues/12

Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

ciao-scheduler/scheduler.go
@@ -83,6 +83,56 @@ type controllerStat struct {
 	uuid   string
 }
 
+func (sched *ssntpSchedulerServer) sendNodeConnectionEvent(nodeUUID, controllerUUID string, nodeType payloads.Resource, connected bool) (int, error) {
+	if connected == true {
+		payload := payloads.NodeConnected{
+			Connected: payloads.NodeConnectedEvent{
+				NodeUUID: nodeUUID,
+				NodeType: nodeType,
+			},
+		}
+
+		b, err := yaml.Marshal(&payload)
+		if err != nil {
+			return 0, err
+		}
+
+		return sched.ssntp.SendEvent(controllerUUID, ssntp.NodeConnected, b)
+	} else {
+		payload := payloads.NodeDisconnected{
+			Disconnected: payloads.NodeConnectedEvent{
+				NodeUUID: nodeUUID,
+				NodeType: nodeType,
+			},
+		}
+
+		b, err := yaml.Marshal(&payload)
+		if err != nil {
+			return 0, err
+		}
+
+		return sched.ssntp.SendEvent(controllerUUID, ssntp.NodeDisconnected, b)
+	}
+}
+
+func (sched *ssntpSchedulerServer) sendNodeConnectedEvents(nodeUUID string, nodeType payloads.Resource) {
+	sched.controllerMutex.Lock()
+	defer sched.controllerMutex.Unlock()
+
+	for _, c := range sched.controllerMap {
+		sched.sendNodeConnectionEvent(nodeUUID, c.uuid, nodeType, true)
+	}
+}
+
+func (sched *ssntpSchedulerServer) sendNodeDisconnectedEvents(nodeUUID string, nodeType payloads.Resource) {
+	sched.controllerMutex.Lock()
+	defer sched.controllerMutex.Unlock()
+
+	for _, c := range sched.controllerMap {
+		sched.sendNodeConnectionEvent(nodeUUID, c.uuid, nodeType, false)
+	}
+}
+
 func (sched *ssntpSchedulerServer) ConnectNotify(uuid string, role uint32) {
 	switch role {
 	case ssntp.Controller:
@@ -124,6 +174,8 @@ func (sched *ssntpSchedulerServer) ConnectNotify(uuid string, role uint32) {
 		node.uuid = uuid
 		sched.cnList = append(sched.cnList, &node)
 		sched.cnMap[uuid] = &node
+
+		sched.sendNodeConnectedEvents(uuid, payloads.ComputeNode)
 	case ssntp.NETAGENT:
 		sched.nnMutex.Lock()
 		defer sched.nnMutex.Unlock()
@@ -137,13 +189,14 @@ func (sched *ssntpSchedulerServer) ConnectNotify(uuid string, role uint32) {
 		node.status = ssntp.CONNECTED
 		node.uuid = uuid
 		sched.nnMap[uuid] = &node
+
+		sched.sendNodeConnectedEvents(uuid, payloads.NetworkNode)
 	}
 
 	glog.V(2).Infof("Connect (role 0x%x, uuid=%s)\n", role, uuid)
 }
 
 func (sched *ssntpSchedulerServer) DisconnectNotify(uuid string) {
-
 	sched.controllerMutex.Lock()
 	defer sched.controllerMutex.Unlock()
 	if sched.controllerMap[uuid] != nil {
@@ -188,6 +241,9 @@ func (sched *ssntpSchedulerServer) DisconnectNotify(uuid string) {
 			sched.cnMRUIndex = -1
 		}
 
+		/* We need a go routine as the controller lock is taken */
+		go sched.sendNodeDisconnectedEvents(uuid, payloads.ComputeNode)
+
 		delete(sched.cnMap, uuid)
 
 		glog.V(2).Infof("Disconnect cn (uuid=%s)\n", uuid)
@@ -197,6 +253,9 @@ func (sched *ssntpSchedulerServer) DisconnectNotify(uuid string) {
 	sched.nnMutex.Lock()
 	defer sched.nnMutex.Unlock()
 	if sched.nnMap[uuid] != nil {
+		/* We need a go routine as the controller lock is taken */
+		go sched.sendNodeDisconnectedEvents(uuid, payloads.NetworkNode)
+
 		//TODO: consider moving to nnInactiveMap?
 		delete(sched.nnMap, uuid)
 

commit 8cf3133fa6262c86cc235d17eeb077382123c404

ciao-scheduler: fix deadlock in connect/disconnect notifiers
Commit a17d38a2 introduced a data deadlock between connect and disconnect
notifiers.  The connect notifier has a role, but disconnect does not.  This
means the disconnect routine acquires and defers the unlock of multiple
maps' protection mutex.

We should probably have the disconnect include a role id, so we only
acquire the one needed lock.  In the meantime:

The maps are read mostly so have a RW lock to protect the map structure.
We take the reader lock in paths which simply traverse the map.  We take
the write lock in paths we know are going to modify the map itself.
Map member objects have their own locks if needed to protect the object's
internal data consistency.  Using a read lock while traversing the map to
forward notifications fixes side of the issue.

And not doing deferred unlocks fixes the other side, though it is better to
use defers if we can add a role id to the disconnect notifier.  Issue #121
opened to track this.

Signed-off-by: Tim Pepper <timothy.c.pepper@linux.intel.com>

ciao-scheduler/scheduler.go
@@ -36,17 +36,17 @@ type ssntpSchedulerServer struct {
 	name  string
 	// Command & Status Reporting node(s)
 	controllerMap   map[string]*controllerStat
-	controllerMutex sync.RWMutex
+	controllerMutex sync.RWMutex // Rlock traversal of map, Lock modification of map
 	// Compute Nodes
 	cnMap      map[string]*nodeStat
 	cnList     []*nodeStat
-	cnMutex    sync.RWMutex
+	cnMutex    sync.RWMutex // Rlock traversal of map, Lock modification of map
 	cnMRU      *nodeStat
 	cnMRUIndex int
 	//cnInactiveMap      map[string]nodeStat
 	// Network Nodes
 	nnMap   map[string]*nodeStat
-	nnMutex sync.RWMutex
+	nnMutex sync.RWMutex // Rlock traversal of map, Lock modification of map
 	nnMRU   string
 }
 
@@ -129,8 +129,8 @@ func (sched *ssntpSchedulerServer) sendNodeConnectionEvent(nodeUUID, controllerU
 }
 
 func (sched *ssntpSchedulerServer) sendNodeConnectedEvents(nodeUUID string, nodeType payloads.Resource) {
-	sched.controllerMutex.Lock()
-	defer sched.controllerMutex.Unlock()
+	sched.controllerMutex.RLock()
+	defer sched.controllerMutex.RUnlock()
 
 	for _, c := range sched.controllerMap {
 		sched.sendNodeConnectionEvent(nodeUUID, c.uuid, nodeType, true)
@@ -138,8 +138,8 @@ func (sched *ssntpSchedulerServer) sendNodeConnectedEvents(nodeUUID string, node
 }
 
 func (sched *ssntpSchedulerServer) sendNodeDisconnectedEvents(nodeUUID string, nodeType payloads.Resource) {
-	sched.controllerMutex.Lock()
-	defer sched.controllerMutex.Unlock()
+	sched.controllerMutex.RLock()
+	defer sched.controllerMutex.RUnlock()
 
 	for _, c := range sched.controllerMap {
 		sched.sendNodeConnectionEvent(nodeUUID, c.uuid, nodeType, false)
@@ -211,7 +211,7 @@ func (sched *ssntpSchedulerServer) ConnectNotify(uuid string, role uint32) {
 
 func (sched *ssntpSchedulerServer) DisconnectNotify(uuid string, role uint32) {
 	sched.controllerMutex.Lock()
-	defer sched.controllerMutex.Unlock()
+	//BUG(121): defer sched.controllerMutex.Unlock()
 	if sched.controllerMap[uuid] != nil {
 		controller := sched.controllerMap[uuid]
 		if controller.status == controllerMaster {
@@ -230,11 +230,15 @@ func (sched *ssntpSchedulerServer) DisconnectNotify(uuid string, role uint32) {
 		delete(sched.controllerMap, uuid)
 
 		glog.V(2).Infof("Disconnect controller (uuid=%s)\n", uuid)
+		//BUG(121): use defer
+		sched.controllerMutex.Unlock()
 		return
 	}
+	//BUG(121): use defer
+	sched.controllerMutex.Unlock()
 
 	sched.cnMutex.Lock()
-	defer sched.cnMutex.Unlock()
+	//BUG(121): defer sched.cnMutex.Unlock()
 	if sched.cnMap[uuid] != nil {
 		//TODO: consider moving to cnInactiveMap?
 		node := sched.cnMap[uuid]
@@ -260,11 +264,15 @@ func (sched *ssntpSchedulerServer) DisconnectNotify(uuid string, role uint32) {
 		delete(sched.cnMap, uuid)
 
 		glog.V(2).Infof("Disconnect cn (uuid=%s)\n", uuid)
+		//BUG(121): use defer
+		sched.cnMutex.Unlock()
 		return
 	}
+	//BUG(121): use defer
+	sched.cnMutex.Unlock()
 
 	sched.nnMutex.Lock()
-	defer sched.nnMutex.Unlock()
+	//BUG(121): defer sched.nnMutex.Unlock()
 	if sched.nnMap[uuid] != nil {
 		/* We need a go routine as the controller lock is taken */
 		go sched.sendNodeDisconnectedEvents(uuid, payloads.NetworkNode)
@@ -273,8 +281,12 @@ func (sched *ssntpSchedulerServer) DisconnectNotify(uuid string, role uint32) {
 		delete(sched.nnMap, uuid)
 
 		glog.V(2).Infof("Disconnect nn (uuid=%s)\n", uuid)
+		//BUG(121): use defer
+		sched.nnMutex.Unlock()
 		return
 	}
+	//BUG(121): use defer
+	sched.nnMutex.Unlock()
 
 	glog.Warningf("Disconnect error: no ssntp client with uuid=%s\n", uuid)
 	return