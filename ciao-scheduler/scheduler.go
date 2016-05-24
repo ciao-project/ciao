fab12bd457d334e53456a6b9b331a3f63e51ced0
Tim Pepper <timothy.c.pepper@linux.intel.com>
2016-05-24 09:55:34 -0700

    ciao-scheduler: refactor StatusNotify() for combo role AGENT|NETAGENT
    Split out a helper to update a nodeStat.
    
    With StatusNotify() including a role parameter, the helper function can be
    simply called for each relevant role, instead of having to check all maps
    for the uuid passed in the already existing StatusNotify() parameter list.
    
    Signed-off-by: Tim Pepper <timothy.c.pepper@linux.intel.com>

--- ciao-scheduler/scheduler.go
+++ ciao-scheduler/scheduler.go
@@ -349,35 +349,8 @@ func (sched *ssntpSchedulerServer) DisconnectNotify(uuid string, role ssntp.Role
 	glog.V(2).Infof("Connect (role 0x%x, uuid=%s)\n", role, uuid)
 }
 
-func (sched *ssntpSchedulerServer) StatusNotify(uuid string, status ssntp.Status, frame *ssntp.Frame) {
+func (sched *ssntpSchedulerServer) updateNodeStat(node *nodeStat, status ssntp.Status, frame *ssntp.Frame) {
 	payload := frame.Payload
-
-	// for now only pay attention to READY status
-
-	glog.V(2).Infof("STATUS %v from %s\n", status, uuid)
-
-	sched.controllerMutex.RLock()
-	defer sched.controllerMutex.RUnlock()
-	if sched.controllerMap[uuid] != nil {
-		glog.Warningf("Ignoring STATUS change from Controller uuid=%s\n", uuid)
-		return
-	}
-
-	sched.cnMutex.RLock()
-	defer sched.cnMutex.RUnlock()
-
-	sched.nnMutex.RLock()
-	defer sched.nnMutex.RUnlock()
-
-	var node *nodeStat
-	if sched.cnMap[uuid] != nil {
-		node = sched.cnMap[uuid]
-	} else if sched.nnMap[uuid] != nil {
-		node = sched.nnMap[uuid]
-	} else {
-		glog.Warningf("STATUS error: no connected ssntp client with uuid=%s\n", uuid)
-		return
-	}
 
 	node.mutex.Lock()
 	defer node.mutex.Unlock()
@@ -389,7 +362,7 @@ func (sched *ssntpSchedulerServer) StatusNotify(uuid string, status ssntp.Status
 		var stats payloads.Ready
 		err := yaml.Unmarshal(payload, &stats)
 		if err != nil {
-			glog.Errorf("Bad READY yaml for node %s\n", uuid)
+			glog.Errorf("Bad READY yaml for node %s\n", node.uuid)
 			return
 		}
 		node.memTotalMB = stats.MemTotalMB
@@ -397,6 +370,38 @@ func (sched *ssntpSchedulerServer) StatusNotify(uuid string, status ssntp.Status
 		node.load = stats.Load
 		node.cpus = stats.CpusOnline
 		//TODO pull in other types of payloads.Ready struct data
+	}
+}
+
+func (sched *ssntpSchedulerServer) StatusNotify(uuid string, status ssntp.Status, frame *ssntp.Frame) {
+	// for now only pay attention to READY status
+
+	role, err := sched.ssntp.ClientRole(uuid)
+	if err != nil {
+		glog.Errorf("STATUS ignored from disconnected client %s", uuid)
+		return
+	}
+
+	glog.V(2).Infof("STATUS %v from %s (%s)\n", status, uuid, role.String())
+
+	if role.IsAgent() {
+		var cn *nodeStat
+		sched.cnMutex.RLock()
+		defer sched.cnMutex.RUnlock()
+		if sched.cnMap[uuid] != nil {
+			cn = sched.cnMap[uuid]
+			sched.updateNodeStat(cn, status, frame)
+		}
+	}
+
+	if role.IsNetAgent() {
+		var nn *nodeStat
+		sched.nnMutex.RLock()
+		defer sched.nnMutex.RUnlock()
+		if sched.nnMap[uuid] != nil {
+			nn = sched.nnMap[uuid]
+			sched.updateNodeStat(nn, status, frame)
+		}
 	}
 }
 
