25e17136a7948af3d77fb98ed0667fde9d809404
Kristen Carlson Accardi <kristen@linux.intel.com>
2016-07-21 14:22:56 -0700

    ciao-storage: make golint clean.
    Comment as neccessary to make golint happy.
    
    Signed-off-by: Kristen Carlson Accardi <kristen@linux.intel.com>

--- ciao-storage/block.go
+++ ciao-storage/block.go
@@ -1,14 +1,19 @@
 package storage
 
+// BlockDriver is the interface that all block drivers must implement.
 type BlockDriver interface {
 	CreateBlockDevice(image *string, sizeGB int) (BlockDevice, error)
 	DeleteBlockDevice(string) error
 }
 
+// BlockDevice contains information about a block devices.
 type BlockDevice struct {
 	ID string
 }
 
+// GetBlockDriver returns an implementation of the BlockDriver interface.
+// For now we are just returning the noopDriver. We should probably switch
+// this to take a string or just use build flags to select.
 func GetBlockDriver() BlockDriver {
 	return noopDriver{}
 }
