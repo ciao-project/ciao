4fb588958da0edd34b8b09a13ced24e4734511a2
Tim Pepper <timothy.c.pepper@linux.intel.com>
2016-08-19 10:40:49 -0700

    testutil: use consistent timeout for results channels
    These timeouts "should never trigger", but if they do we get a test
    failure sooner than waiting for go test's very long timeout.  The 25
    second number isn't special.  And it's known to possibly be too short
    for example in the case of TestReconnects(), where the ssntp client
    incurs a series of delays that are 0..5, 0..10, 0..20, 0..40 seconds.
    If the server is slow in restarting it _might_ take 4 seconds to restart
    in travis.  But worst case we hit client delays of 1,1,1,40 seconds.
    In which case despite everything "working correctly" the test case will
    timeout for lack of NodeConnected result after 25s, when it would be
    coming at 43s.
    
    Signed-off-by: Tim Pepper <timothy.c.pepper@linux.intel.com>

--- testutil/agent.go
+++ testutil/agent.go
@@ -120,7 +120,7 @@ func (client *SsntpTestClient) GetCmdChanResult(c *chan Result, cmd ssntp.Comman
 		if result.Err != nil {
 			err = fmt.Errorf("Client error sending %s command: %s\n", cmd, result.Err)
 		}
-	case <-time.After(5 * time.Second):
+	case <-time.After(25 * time.Second):
 		err = fmt.Errorf("Timeout waiting for client %s command result\n", cmd)
 	}
 
@@ -159,7 +159,7 @@ func (client *SsntpTestClient) GetEventChanResult(c *chan Result, evt ssntp.Even
 		if result.Err != nil {
 			err = fmt.Errorf("Client error sending %s event: %s\n", evt, result.Err)
 		}
-	case <-time.After(20 * time.Second):
+	case <-time.After(25 * time.Second):
 		err = fmt.Errorf("Timeout waiting for client %s event result\n", evt)
 	}
 
@@ -198,7 +198,7 @@ func (client *SsntpTestClient) GetErrorChanResult(c *chan Result, error ssntp.Er
 		if result.Err != nil {
 			err = fmt.Errorf("Client error sending %s error: %s\n", error, result.Err)
 		}
-	case <-time.After(20 * time.Second):
+	case <-time.After(25 * time.Second):
 		err = fmt.Errorf("Timeout waiting for client %s error result\n", error)
 	}
 
@@ -237,7 +237,7 @@ func (client *SsntpTestClient) GetStatusChanResult(c *chan Result, status ssntp.
 		if result.Err != nil {
 			err = fmt.Errorf("Client error sending %s status: %s\n", status, result.Err)
 		}
-	case <-time.After(5 * time.Second):
+	case <-time.After(25 * time.Second):
 		err = fmt.Errorf("Timeout waiting for client %s status result\n", status)
 	}
 
