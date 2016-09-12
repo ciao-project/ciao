ad1649f6ca3d9659e2241535bc21745349f20280
Samuel Ortiz <sameo@linux.intel.com>
2016-09-12 15:02:17 +0200

    qemu: Add configuration structure to simplify LaunchQemu
    LaunchQemu() now takes a Config structure that contains some more
    descriptive fields than raw qemu parameters but also some extra
    parameters. LaunchQemu() will combine those into a slice of paremeters
    for LaunchCustomQemu() to consume.
    
    LaunchQemu is now simpler to call and more extensible as supporting more
    qemu parameters would mean expanding Config instead of changing the API.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- qemu/examples_test.go
+++ qemu/examples_test.go
@@ -37,10 +37,15 @@ func Example() {
 	// resources
 	params = append(params, "-m", "370", "-smp", "cpus=2")
 
+	config := qemu.Config{
+		Ctx:         context.Background(),
+		ExtraParams: params,
+	}
+
 	// LaunchQemu should return as soon as the instance has launched as we
 	// are using the --daemonize flag.  It will set up a unix domain socket
 	// called /tmp/qmp-socket that we can use to manage the instance.
-	_, err := qemu.LaunchQemu(context.Background(), params, nil, nil)
+	_, err := qemu.LaunchQemu(config, nil)
 	if err != nil {
 		panic(err)
 	}
