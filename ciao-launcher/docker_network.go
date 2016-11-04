ciao-project/ciao

commit 54d8e50b2657efd45e02f53fda7bccc3a514ff04

ciao-launcher: Fix docker network locks
This commit fixes #763, an issue that was introduced by some poor optimization.
When we create a container on a subnet on a CN for the first time we need
to create a new docker network.  This network needs to be created after the vnic
but before the container is created.  It follows that we need to be careful when
creating multiple containers in parallel.  The docker network will be created
during the creation of one of these containers.  The instance go routines
controlling the creation of the other containers need to block until the network
has been created, otherwise they will fail.  The optimisation deleted by this PR
was trying to reduce the amount of time the lock would be held for.
Unfortunately, the code did not consider the case that when instances are
deleted, the final instance deleted for a subnet on a CN might be a VM and not a
container.  When this happens, it becomes impossible to start a new container
for that subnet as the container code thinks that the network for that subnet
already exists, when in fact the underlying bridge was deleted along with the
VM.

This commit fixes the problem by removing the optimisation and placing Vnic
and docker network creation inside a critical section.  As we do not currently
allow parallel vnic creation, this shoudln't actually slow anything down.

Fixes #763

Signed-off-by: Mark Ryan <mark.d.ryan@intel.com>

ciao-launcher/docker_network.go
@@ -32,41 +32,13 @@ type dockerNetworkState struct {
 	err  error
 }
 
-var dockerNetworkMap struct {
-	sync.Mutex
-	networks map[string]*dockerNetworkState
-}
-
-func init() {
-	dockerNetworkMap.networks = make(map[string]*dockerNetworkState)
-}
+var dockerNetworkLock sync.Mutex
 
 func createDockerVnic(vnicCfg *libsnnet.VnicConfig) (*libsnnet.Vnic, *libsnnet.SsntpEventInfo, *libsnnet.ContainerInfo, error) {
-	dockerNetworkMap.Lock()
-	state := dockerNetworkMap.networks[vnicCfg.SubnetID]
-	if state != nil {
-		dockerNetworkMap.Unlock()
-		glog.Info("Waiting for Docker network creation")
-		<-state.done
-		if state.err != nil {
-			return nil, nil, nil, state.err
-		}
-
-		return cnNet.CreateVnic(vnicCfg)
-	}
-	ch := make(chan struct{})
-	defer close(ch)
-	state = &dockerNetworkState{done: ch}
-	dockerNetworkMap.networks[vnicCfg.SubnetID] = state
-	dockerNetworkMap.Unlock()
+	dockerNetworkLock.Lock()
+	defer dockerNetworkLock.Unlock()
 	vnic, event, info, err := cnNet.CreateVnic(vnicCfg)
-	state.err = err
 	if err != nil {
-		return vnic, event, info, err
-	}
-
-	if info == nil {
-		glog.Warning("VNIC information expected")
 		return vnic, event, info, err
 	}
 
@@ -77,7 +49,7 @@ func createDockerVnic(vnicCfg *libsnnet.VnicConfig) (*libsnnet.Vnic, *libsnnet.S
 	// the container will not launch.
 
 	_ = createDockerNetwork(context.Background(), info)
-	return vnic, event, info, state.err
+	return vnic, event, info, nil
 }
 
 func destroyDockerVnic(vnicCfg *libsnnet.VnicConfig) (*libsnnet.SsntpEventInfo, error) {
@@ -96,9 +68,6 @@ func destroyDockerVnic(vnicCfg *libsnnet.VnicConfig) (*libsnnet.SsntpEventInfo, 
 		// these inconsistencies and cleans up:
 		// https://github.com/01org/ciao/issues/4
 		_ = destroyDockerNetwork(context.Background(), info.SubnetID)
-		dockerNetworkMap.Lock()
-		delete(dockerNetworkMap.networks, vnicCfg.SubnetID)
-		dockerNetworkMap.Unlock()
 	}
 
 	return event, err

commit b7cabb8ac1094a60a63c05f935dd6c7d6496861f

ciao-launcher: Stop leaking docker networks
There were two issues here.  Commit 54d8e50 introduced a bug which
lead to a new docker network being created with every container,
which is not what we want.  The intended behaviour is to create a
new docker network for each new subnet.  This creation of extra
networks lead to a leak as they were not getting deleted as they
were not supposed to exist.

There was an existing bug which could result in docker network
leaks when you created a container, a VM, deleted the container
and then the VM in that order.

Both these issues have now been fixed.

Fixes #768

Signed-off-by: Mark Ryan <mark.d.ryan@intel.com>

ciao-launcher/docker_network.go
@@ -38,7 +38,7 @@ func createDockerVnic(vnicCfg *libsnnet.VnicConfig) (*libsnnet.Vnic, *libsnnet.S
 	dockerNetworkLock.Lock()
 	defer dockerNetworkLock.Unlock()
 	vnic, event, info, err := cnNet.CreateVnic(vnicCfg)
-	if err != nil {
+	if info.CNContainerEvent != libsnnet.ContainerNetworkAdd || err != nil {
 		return vnic, event, info, err
 	}
 
@@ -50,27 +50,6 @@ func createDockerVnic(vnicCfg *libsnnet.VnicConfig) (*libsnnet.Vnic, *libsnnet.S
 
 	_ = createDockerNetwork(context.Background(), info)
 	return vnic, event, info, nil
-}
-
-func destroyDockerVnic(vnicCfg *libsnnet.VnicConfig) (*libsnnet.SsntpEventInfo, error) {
-	// BUG(markus): We need to pass in a context to destroyVnic
-
-	event, info, err := cnNet.DestroyVnic(vnicCfg)
-	if err != nil {
-		glog.Errorf("cn.DestroyVnic failed %v", err)
-		return event, err
-	}
-
-	if info != nil {
-		// This is one of these weird cases we will have with
-		// docker in which some launcher and libssnet state gets out of
-		// sync with docker.  Launcher needs a cleanup routine that detects
-		// these inconsistencies and cleans up:
-		// https://github.com/01org/ciao/issues/4
-		_ = destroyDockerNetwork(context.Background(), info.SubnetID)
-	}
-
-	return event, err
 }
 
 func createDockerNetwork(ctx context.Context, info *libsnnet.ContainerInfo) error {

ciao-launcher/network.go
@@ -295,17 +295,19 @@ func createVnic(conn serverConn, vnicCfg *libsnnet.VnicConfig) (string, string, 
 
 func destroyVnic(conn serverConn, vnicCfg *libsnnet.VnicConfig) error {
 	if vnicCfg.VnicRole != libsnnet.DataCenter {
-		var event *libsnnet.SsntpEventInfo
-		var err error
-
-		if vnicCfg.VnicRole == libsnnet.TenantContainer {
-			event, err = destroyDockerVnic(vnicCfg)
-		} else {
-			event, _, err = cnNet.DestroyVnic(vnicCfg)
-		}
+		event, info, err := cnNet.DestroyVnic(vnicCfg)
 		if err != nil {
 			glog.Errorf("cn.DestroyVnic failed %v", err)
 			return err
+		}
+
+		if info != nil && info.CNContainerEvent == libsnnet.ContainerNetworkDel {
+			// This is one of these weird cases we will have with
+			// docker in which some launcher and libssnet state gets out of
+			// sync with docker.  Launcher needs a cleanup routine that detects
+			// these inconsistencies and cleans up:
+			// https://github.com/01org/ciao/issues/4
+			_ = destroyDockerNetwork(context.Background(), info.SubnetID)
 		}
 
 		sendNetworkEvent(conn, ssntp.TenantRemoved, event)