66f3ab762bb066b7b80f278583d50095b553cc50
Mark Ryan <mark.d.ryan@intel.com>
2016-12-09 14:30:37 +0000

    ciao-launcher: Add additional payload tests
    This commit adds the following unit tests:
    
    - TestParseStartPayload
    - TestGenerateNetEventPayload
    - TestParseRestartPayload
    - TestParseDeletePayload
    - TestParseStopPayload
    
    Signed-off-by: Mark Ryan <mark.d.ryan@intel.com>

--- ciao-launcher/payload_test.go
+++ ciao-launcher/payload_test.go
@@ -17,11 +17,151 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 
+	yaml "gopkg.in/yaml.v2"
+
+	"github.com/01org/ciao/networking/libsnnet"
 	"github.com/01org/ciao/payloads"
 	"github.com/01org/ciao/testutil"
 )
+
+var startTests = []struct {
+	payload string
+	config  *vmConfig
+}{
+	{
+		`
+start:
+  requested_resources:
+     - type: vcpus
+       value: 2
+     - type: mem_mb
+       value: 370
+     - type: disk_mb
+       value: 8000
+  instance_uuid: d7d86208-b46c-4465-9018-ee14087d415f
+  tenant_uuid: 67d86208-000-4465-9018-fe14087d415f
+  fw_type: legacy
+  vm_type: qemu
+  networking:
+    vnic_mac: 02:00:e6:f5:af:f9
+    vnic_uuid: 67d86208-b46c-0000-9018-fe14087d415f
+    concentrator_ip: 192.168.42.21
+    concentrator_uuid: 67d86208-b46c-4465-0000-fe14087d415f
+    subnet: 192.168.8.0/21
+    private_ip: 192.168.8.2
+  storage:
+    id: 69e84267-ed01-4738-b15f-b47de06b62e7
+    boot: true
+`,
+		&vmConfig{
+			Cpus:       2,
+			Mem:        370,
+			Disk:       8000,
+			Instance:   "d7d86208-b46c-4465-9018-ee14087d415f",
+			Legacy:     true,
+			VnicMAC:    "02:00:e6:f5:af:f9",
+			VnicIP:     "192.168.8.2",
+			ConcIP:     "192.168.42.21",
+			SubnetIP:   "192.168.8.0/21",
+			TenantUUID: "67d86208-000-4465-9018-fe14087d415f",
+			ConcUUID:   "67d86208-b46c-4465-0000-fe14087d415f",
+			VnicUUID:   "67d86208-b46c-0000-9018-fe14087d415f",
+			SSHPort:    35050,
+			Volumes: []volumeConfig{
+				{
+					"69e84267-ed01-4738-b15f-b47de06b62e7",
+					true,
+				},
+			},
+		},
+	},
+	{
+		"start",
+		nil,
+	},
+	{
+		`
+start:
+  requested_resources:
+     - type: vcpus
+       value: 2
+     - type: mem_mb
+       value: 370
+     - type: disk_mb
+       value: 8000
+  instance_uuid: imnotvalid
+  tenant_uuid: 67d86208-000-4465-9018-fe14087d415f
+  fw_type: legacy
+  networking:
+    vnic_mac: 02:00:e6:f5:af:f9
+    vnic_uuid: 67d86208-b46c-0000-9018-fe14087d415f
+    concentrator_ip: 192.168.42.21
+    concentrator_uuid: 67d86208-b46c-4465-0000-fe14087d415f
+    subnet: 192.168.8.0/21
+    private_ip: 192.168.8.2
+  storage:
+    id: 69e84267-ed01-4738-b15f-b47de06b62e7
+    boot: true
+`,
+		nil,
+	},
+	{
+		`
+start:
+  requested_resources:
+     - type: vcpus
+       value: 2
+     - type: mem_mb
+       value: 370
+     - type: disk_mb
+       value: 8000
+  instance_uuid: d7d86208-b46c-4465-9018-ee14087d415f
+  tenant_uuid: 67d86208-000-4465-9018-fe14087d415f
+  fw_type: imnotvalid
+  networking:
+    vnic_mac: 02:00:e6:f5:af:f9
+    vnic_uuid: 67d86208-b46c-0000-9018-fe14087d415f
+    concentrator_ip: 192.168.42.21
+    concentrator_uuid: 67d86208-b46c-4465-0000-fe14087d415f
+    subnet: 192.168.8.0/21
+    private_ip: 192.168.8.2
+  storage:
+    id: 69e84267-ed01-4738-b15f-b47de06b62e7
+    boot: true
+`,
+		nil,
+	},
+	{
+		`
+start:
+  requested_resources:
+     - type: vcpus
+       value: 2
+     - type: mem_mb
+       value: 370
+     - type: disk_mb
+       value: 8000
+  instance_uuid: d7d86208-b46c-4465-9018-ee14087d415f
+  vm_type: askajajlsj
+  tenant_uuid: 67d86208-000-4465-9018-fe14087d415f
+  fw_type: legacy
+  networking:
+    vnic_mac: 02:00:e6:f5:af:f9
+    vnic_uuid: 67d86208-b46c-0000-9018-fe14087d415f
+    concentrator_ip: 192.168.42.21
+    concentrator_uuid: 67d86208-b46c-4465-0000-fe14087d415f
+    subnet: 192.168.8.0/21
+    private_ip: 192.168.8.2
+  storage:
+    id: 69e84267-ed01-4738-b15f-b47de06b62e7
+    boot: true
+`,
+		nil,
+	},
+}
 
 func TestParseAttachVolumePayload(t *testing.T) {
 	instance, volume, err := parseAttachVolumePayload([]byte(testutil.AttachVolumeYaml))
@@ -60,5 +200,114 @@ func TestParseDetachVolumePayload(t *testing.T) {
 	_, _, err = parseDetachVolumePayload([]byte(testutil.BadDetachVolumeYaml))
 	if err == nil || err.code != payloads.DetachVolumeInvalidData {
 		t.Fatalf("DetachVolumeInvalidData error expected")
+	}
+}
+
+func TestParseStartPayload(t *testing.T) {
+	for i, st := range startTests {
+		cfg, err := parseStartPayload([]byte(st.payload))
+		if st.config == nil {
+			if cfg != nil {
+				t.Errorf("Expected nil config due to bad payload %d", i)
+			}
+			continue
+		}
+		if err != nil {
+			t.Errorf("Failed to parse payload %d : %v", i, err)
+			continue
+		}
+		if !reflect.DeepEqual(cfg, st.config) {
+			t.Errorf("Start payload %d does not match", i)
+		}
+	}
+}
+
+func compareNetEvents(t *testing.T, ev *libsnnet.SsntpEventInfo, eventData *payloads.TenantAddedEvent) {
+	if eventData.AgentUUID != testutil.AgentUUID ||
+		eventData.AgentIP != ev.CnIP ||
+		eventData.TenantUUID != ev.TenantID ||
+		eventData.TenantSubnet != ev.SubnetID ||
+		eventData.ConcentratorUUID != ev.ConcID ||
+		eventData.ConcentratorIP != ev.CnciIP ||
+		eventData.SubnetKey != ev.SubnetKey {
+		t.Errorf("payloads and ssntp events do not match")
+	}
+}
+
+func TestGenerateNetEventPayload(t *testing.T) {
+	ev := &libsnnet.SsntpEventInfo{
+		Event:     libsnnet.SsntpTunAdd,
+		CnciIP:    testutil.CNCIIP,
+		CnIP:      testutil.InstancePrivateIP,
+		Subnet:    testutil.TenantSubnet,
+		TenantID:  testutil.TenantUUID,
+		SubnetID:  "",
+		ConcID:    testutil.CNCIUUID,
+		CnID:      testutil.AgentUUID,
+		SubnetKey: 1,
+	}
+
+	pl, err := generateNetEventPayload(ev, testutil.AgentUUID)
+	if err != nil {
+		t.Fatalf("Failed to generate payload : %v", err)
+	}
+	var eventData payloads.EventTenantAdded
+	err = yaml.Unmarshal(pl, &eventData)
+	if err != nil {
+		t.Fatalf("Unable to unmarshall event : %v", err)
+	}
+
+	compareNetEvents(t, ev, &eventData.TenantAdded)
+
+	ev.Event = libsnnet.SsntpTunDel
+	pl, err = generateNetEventPayload(ev, testutil.AgentUUID)
+	if err != nil {
+		t.Fatalf("Failed to generate payload : %v", err)
+	}
+	var eventData2 payloads.EventTenantRemoved
+	err = yaml.Unmarshal(pl, &eventData2)
+	if err != nil {
+		t.Fatalf("Unable to unmarshall event : %v", err)
+	}
+
+	compareNetEvents(t, ev, &eventData2.TenantRemoved)
+
+	ev.Event = 666
+	_, err = generateNetEventPayload(ev, testutil.AgentUUID)
+	if err == nil {
+		t.Errorf("generateNetEventPayload should have failed on invalid event type")
+	}
+}
+
+func TestParseRestartPayload(t *testing.T) {
+	instance, err := parseRestartPayload([]byte(testutil.RestartYaml))
+	if err != nil {
+		t.Fatalf("Failed to parse restart payload : %v", err.err)
+	}
+	if instance != testutil.InstanceUUID {
+		t.Errorf("Wrong instance UUID.  Expected %s found %s", instance,
+			testutil.InstanceUUID)
+	}
+}
+
+func TestParseDeletePayload(t *testing.T) {
+	instance, err := parseDeletePayload([]byte(testutil.DeleteYaml))
+	if err != nil {
+		t.Fatalf("Failed to parse delete payload : %v", err.err)
+	}
+	if instance != testutil.InstanceUUID {
+		t.Errorf("Wrong instance UUID.  Expected %s found %s", instance,
+			testutil.InstanceUUID)
+	}
+}
+
+func TestParseStopPayload(t *testing.T) {
+	instance, err := parseStopPayload([]byte(testutil.StopYaml))
+	if err != nil {
+		t.Fatalf("Failed to parse stop payload : %v", err.err)
+	}
+	if instance != testutil.InstanceUUID {
+		t.Errorf("Wrong instance UUID.  Expected %s found %s", instance,
+			testutil.InstanceUUID)
 	}
 }
