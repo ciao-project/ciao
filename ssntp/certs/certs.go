7148ecada6724648de7cbe44f5b6e70b6e3104f3
Rob Bradford <robert.bradford@intel.com>
2017-05-30 18:08:04 +0100

    ssntp/certs: Add operations on certificate signing requests (CSRs)
    This change adds support for generating CSRs, signing a supplied CSR
    into a certificate and merging a signed certificate with a private key
    into the format ciao expects to use for ssntp.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- ssntp/certs/certs.go
+++ ssntp/certs/certs.go
@@ -40,7 +40,10 @@ import (
 	"net"
 	"time"
 
+	"io/ioutil"
+
 	"github.com/01org/ciao/ssntp"
+	"github.com/pkg/errors"
 )
 
 func publicKey(priv interface{}) interface{} {
@@ -141,7 +144,7 @@ func CreateCertTemplate(role ssntp.Role, organization string, email string, host
 	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
 	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
 	if err != nil {
-		return nil, fmt.Errorf("Gailed to generate certificate serial number: %v", err)
+		return nil, fmt.Errorf("Failed to generate certificate serial number: %v", err)
 	}
 
 	template := x509.Certificate{
@@ -248,6 +251,179 @@ func CreateCert(template *x509.Certificate, useElliptic bool, anchorCert []byte,
 	err = pem.Encode(certOutput, block)
 	if err != nil {
 		return fmt.Errorf("Unable to encode PEM block: %v", err)
+	}
+
+	return nil
+}
+
+func createCertTemplateFromCSR(role ssntp.Role, request *x509.CertificateRequest) (*x509.Certificate, error) {
+	notBefore := time.Now()
+	notAfter := notBefore.Add(365 * 24 * time.Hour)
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to generate certificate serial number")
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject:      request.Subject,
+		NotBefore:    notBefore,
+		NotAfter:     notAfter,
+
+		Signature:          request.Signature,
+		SignatureAlgorithm: request.SignatureAlgorithm,
+		PublicKeyAlgorithm: request.PublicKeyAlgorithm,
+		PublicKey:          request.PublicKey,
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageAny},
+		EmailAddresses:        request.EmailAddresses,
+		BasicConstraintsValid: true,
+	}
+
+	template.DNSNames = request.DNSNames
+	template.IPAddresses = request.IPAddresses
+	template.UnknownExtKeyUsage = addOIDs(role, template.UnknownExtKeyUsage)
+	return &template, nil
+}
+
+// CreateCertFromCSR creates a certificate from a CSR signed by the given anchor certificate. It is written in PEM format.
+func CreateCertFromCSR(role ssntp.Role, csr []byte, anchorCert []byte, certOutput io.Writer) error {
+	// Parent public key first
+	certBlock, rest := pem.Decode(anchorCert)
+	parentCert, err := x509.ParseCertificate(certBlock.Bytes)
+	if err != nil {
+		return errors.Wrap(err, "Unable to parse anchor cert")
+	}
+
+	// Parent private key
+	privKeyBlock, _ := pem.Decode(rest)
+	if privKeyBlock == nil {
+		return errors.Wrap(err, "Unable to extract private key from anchor cert")
+	}
+
+	anchorPrivKey, err := keyFromPemBlock(privKeyBlock)
+	if err != nil {
+		return errors.Wrap(err, "Unable to parse private key from anchor cert")
+	}
+
+	// Decode and parse csr
+	csrBlock, _ := pem.Decode(csr)
+	request, err := x509.ParseCertificateRequest(csrBlock.Bytes)
+	if err != nil {
+		return errors.Wrap(err, "Unable to parse CSR")
+	}
+
+	// Check signature on request (proves that creator of csr has private key)
+	err = request.CheckSignature()
+	if err != nil {
+		return errors.Wrap(err, "Signature check on request failed")
+	}
+
+	template, err := createCertTemplateFromCSR(role, request)
+	if err != nil {
+		return errors.Wrap(err, "Unable to create template from CSR")
+	}
+
+	// Create certificate signed by private key from anchorCert
+	derBytes, err := x509.CreateCertificate(rand.Reader, template, parentCert, request.PublicKey, anchorPrivKey)
+	if err != nil {
+		return errors.Wrap(err, "Unable to create certificate")
+	}
+
+	// Write out certificate
+	err = pem.Encode(certOutput, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	if err != nil {
+		return errors.Wrap(err, "Unable to encode PEM block")
+	}
+
+	return nil
+}
+
+// CreateCertificateRequest creates a certificate request template from the supplied details.
+func CreateCertificateRequest(useElliptic bool, organization string, email string, hosts []string, mgmtIPs []string) *x509.CertificateRequest {
+	request := x509.CertificateRequest{
+		Subject: pkix.Name{
+			Organization: []string{organization},
+		},
+
+		EmailAddresses: []string{email},
+	}
+
+	request.DNSNames = addDNSNames(hosts, request.DNSNames)
+	request.IPAddresses = addMgmtIPs(mgmtIPs, request.IPAddresses)
+
+	request.SignatureAlgorithm = x509.SHA1WithRSA
+	if useElliptic {
+		request.SignatureAlgorithm = x509.ECDSAWithSHA1
+	}
+
+	return &request
+}
+
+// CreateCSR creates a CSR from the incoming template for a newly generated private key.
+func CreateCSR(template *x509.CertificateRequest, useElliptic bool, csrOutput io.Writer, privKeyOutput io.Writer) error {
+	// Generate private key
+	priv, err := generatePrivateKey(useElliptic)
+	if err != nil {
+		return errors.Wrap(err, "error generating private key")
+	}
+
+	privBlock, err := pemBlockForKey(priv)
+	if err != nil {
+		return errors.Wrap(err, "error creating pem block for private key")
+	}
+
+	// Send private key to caller to save
+	err = pem.Encode(privKeyOutput, privBlock)
+	if err != nil {
+		return errors.Wrap(err, "error encoding private key")
+	}
+
+	// Create csr
+	csr, err := x509.CreateCertificateRequest(rand.Reader, template, priv)
+	if err != nil {
+		return errors.Wrap(err, "error creating certificate request")
+	}
+
+	// Send csr to calle to pass on
+	err = pem.Encode(csrOutput, &pem.Block{
+		Type:  "CERTIFICATE REQUEST",
+		Bytes: csr,
+	})
+	if err != nil {
+		return errors.Wrap(err, "error creating pem block for csr")
+	}
+
+	return nil
+}
+
+// AddPrivateKeyToCert adds a private key to existing certificate (created from signing a CSR)
+func AddPrivateKeyToCert(certInput io.Reader, privKeyInput io.Reader, certOutput io.Writer) error {
+	certData, err := ioutil.ReadAll(certInput)
+	if err != nil {
+		return errors.Wrap(err, "error reading certificate data")
+	}
+
+	privData, err := ioutil.ReadAll(privKeyInput)
+	if err != nil {
+		return errors.Wrap(err, "error reading private key")
+	}
+
+	certBlock, _ := pem.Decode(certData)
+	privBlock, _ := pem.Decode(privData)
+
+	// Write out certificate (including private key)
+	err = pem.Encode(certOutput, certBlock)
+	if err != nil {
+		return errors.Wrap(err, "Unable to encode PEM block")
+	}
+
+	err = pem.Encode(certOutput, privBlock)
+	if err != nil {
+		return errors.Wrap(err, "Unable to encode PEM block")
 	}
 
 	return nil
