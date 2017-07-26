8724adfd7f07a3819a4bc65c6b048d850eeadf1e
Rob Bradford <robert.bradford@intel.com>
2017-07-26 18:35:12 +0100

    ciao-deploy: Add "ciao-deploy create-cnci" command
    This command will download the CNCI base image, create a signed
    certificate and copy that certificate and the agent binary into an image
    that it has mounted via loopback. When the image is prepared it will be
    uploaded into the image service.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

---
+++ ciao-deploy/cmd/create-cnci.go
@@ -0,0 +1,63 @@
+// Copyright © 2017 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/01org/ciao/ciao-deploy/deploy"
+	"github.com/spf13/cobra"
+)
+
+var anchorCertPath string
+var caCertPath string
+
+// createCNCICmd represents the create-cnci command
+var createCNCICmd = &cobra.Command{
+	Use:   "create-cnci",
+	Short: "Populate cluster with CNCI image",
+	Long: `Downloads the image needed for the CNCI, makes local customisations and
+	 uploads to server`,
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx, cancelFunc := context.WithCancel(context.Background())
+		defer cancelFunc()
+
+		sigCh := make(chan os.Signal, 1)
+		go func() {
+			<-sigCh
+			cancelFunc()
+		}()
+		signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+		err := deploy.CreateCNCIImage(ctx, anchorCertPath, caCertPath, imageCacheDirectory)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating CNCI: %v\n", err)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(createCNCICmd)
+
+	createCNCICmd.Flags().StringVar(&anchorCertPath, "anchor-cert-path", "", "Path to anchor certificate")
+	createCNCICmd.Flags().StringVar(&caCertPath, "ca-cert-path", "", "Path to CA certificate")
+	createCNCICmd.Flags().StringVar(&imageCacheDirectory, "image-cache-directory", "", "Directory to use for caching of downloaded images")
+}
