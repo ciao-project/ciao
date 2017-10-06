1c4a091357bdc2ba1cf71f5b6ab66c11d0decd3b
Rob Bradford <robert.bradford@intel.com>
2017-10-06 12:05:46 +0100

    singlevm: Move singlevm to use ciao-deploy setup
    This makes our development environment match our physical cluster
    environment.
    
    Signed-off-by: Rob Bradford <robert.bradford@intel.com>

--- testutil/singlevm/cleanup.sh
+++ testutil/singlevm/cleanup.sh
@@ -5,9 +5,9 @@
 ciao_gobin="$GOPATH"/bin
 ciao_host=$(hostname)
 ext_int=$(ip -o route get 8.8.8.8 | cut -d ' ' -f 5)
-sudo killall ciao-scheduler
-sudo killall ciao-controller
-sudo killall ciao-launcher
+sudo systemctl stop ciao-scheduler
+sudo systemctl stop ciao-controller
+sudo systemctl stop ciao-launcher
 sleep 2
 sudo "$ciao_gobin"/ciao-launcher --alsologtostderr -v 3 --hard-reset
 sudo iptables -D FORWARD -i ciao_br -j ACCEPT
@@ -20,4 +20,4 @@ sudo ip link del ciao_br
 sudo pkill -F /tmp/dnsmasq.ciaovlan.pid
 sudo docker rm -v -f ceph-demo
 sudo rm /etc/ceph/*
-sudo rm -rf /var/lib/ciao/ciao-image
+sudo rm -rf /var/lib/ciao
