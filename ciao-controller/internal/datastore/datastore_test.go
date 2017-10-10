ecabdc72a2ccfbb665ded1fac5cf4fe8015a0061
Mark Ryan <mark.d.ryan@intel.com>
2017-10-10 17:36:10 +0100

    ciao-controller: Don't update volumes on stats reception
    There was a race condition in controller that was leading to intermittent
    storage BAT test failures.  The issue occured if a STATS update was received
    for an instance while controller was trying to attach a volume to that
    instance.  The STATS update does not contain information about the volume
    if launcher has not yet processed the Attach command.  Controller was
    noticing that the volume was missing from the STATS and mistakenly deleting it.
    This is no longer necessary now that live volume detatch has been dropped as
    a feature.
    
    Signed-off-by: Mark Ryan <mark.d.ryan@intel.com>

--- ciao-controller/internal/datastore/datastore_test.go
+++ ciao-controller/internal/datastore/datastore_test.go
@@ -1560,113 +1560,6 @@ func TestCreateStorageAttachment(t *testing.T) {
 	}
 }
 
-func TestUpdateStorageAttachmentExisting(t *testing.T) {
-	tenant, err := addTestTenant()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	blockDevice := storage.BlockDevice{
-		ID: "validID",
-	}
-
-	data := types.BlockData{
-		BlockDevice: blockDevice,
-		State:       types.Available,
-		TenantID:    tenant.ID,
-		CreateTime:  time.Now(),
-	}
-
-	err = ds.AddBlockDevice(data)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	wls, err := ds.GetWorkloads(tenant.ID)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(wls) == 0 {
-		t.Fatal("No Workloads Found")
-	}
-
-	instance, err := addTestInstance(tenant, wls[0])
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	volume := payloads.StorageResource{
-		ID:        data.ID,
-		Ephemeral: false,
-		Bootable:  false,
-	}
-	_, err = ds.CreateStorageAttachment(instance.ID, volume)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// get the attachments associated with this instance
-	a1 := ds.GetStorageAttachments(instance.ID)
-
-	if len(a1) != 1 {
-		t.Fatal(err)
-	}
-
-	if a1[0].InstanceID != instance.ID {
-		t.Fatal(err)
-	}
-
-	attachments := []string{data.ID}
-
-	// this doesn't return an error, but we can still exercise
-	// the code to see if anything panics.
-	ds.updateStorageAttachments(instance.ID, attachments)
-}
-
-func TestUpdateStorageAttachmentNotExisting(t *testing.T) {
-	tenant, err := addTestTenant()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	blockDevice := storage.BlockDevice{
-		ID: "validID",
-	}
-
-	data := types.BlockData{
-		BlockDevice: blockDevice,
-		State:       types.Available,
-		TenantID:    tenant.ID,
-		CreateTime:  time.Now(),
-	}
-
-	err = ds.AddBlockDevice(data)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	wls, err := ds.GetWorkloads(tenant.ID)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(wls) == 0 {
-		t.Fatal("No Workloads Found")
-	}
-
-	instance, err := addTestInstance(tenant, wls[0])
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	attachments := []string{data.ID}
-
-	// this doesn't return an error, but we can still exercise
-	// the code to see if anything panics.
-	ds.updateStorageAttachments(instance.ID, attachments)
-}
-
 func TestUpdateStorageAttachmentDeleted(t *testing.T) {
 	tenant, err := addTestTenant()
 	if err != nil {
@@ -1713,12 +1606,7 @@ func TestUpdateStorageAttachmentDeleted(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	attachments := []string{}
-
-	// this doesn't return an error, but we can still exercise
-	// the code to see if anything panics.
-	// send in an empty list.
-	ds.updateStorageAttachments(instance.ID, attachments)
+	ds.updateStorageAttachments(instance.ID)
 }
 
 func TestGetStorageAttachment(t *testing.T) {
