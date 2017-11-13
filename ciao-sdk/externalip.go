c745d818604273ffb02ec3eb13b0ffd072f5f1ab
Tudor Marcu <tudor.marcu@intel.com>
2017-11-12 21:07:44 -0800

    ciao-sdk: Add show/list externalip
    Signed-off-by: Tudor Marcu <tudor.marcu@intel.com>

---
+++ ciao-sdk/externalip.go
@@ -0,0 +1,16 @@
+package sdk
+
+import (
+	"github.com/ciao-project/ciao/ciao-controller/types"
+	"github.com/ciao-project/ciao/client"
+	"github.com/pkg/errors"
+)
+
+func ListExternalIP(c *client.Client, flags CommandOpts) ([]types.MappedIP, error) {
+	IPs, err := c.ListExternalIPs()
+	if err != nil {
+		return nil, errors.Wrap(err, "Error listing external IPs")
+	}
+
+	return IPs, err
+}
